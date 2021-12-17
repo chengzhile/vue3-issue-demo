a00f0f786e688834a4625baff505fe3937af9012
chengzhile <7354862+zhixuli@user.noreply.gitee.com>
2021-12-17 11:12:01 +0800

    el-select-v2验证无效demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <el-form ref="formRef" :model="submitForm" :rules="formRules">
+    <el-form-item label="选项" prop="option">
+      <el-select-v2 clearable v-model="submitForm.option" :options="options" />
+    </el-form-item>
+  </el-form>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+
+  name: 'home',
+
+  setup () {
+    const formRef = ref()
+
+    return {
+      formRef
+    }
+  },
+
+  data () {
+    return {
+      formRules: {
+        option: [
+          {
+            required: true,
+            message: '请选择',
+            trigger: 'change'
+          }
+        ]
+      },
+
+      submitForm: {
+        option: ''
+
+      },
+      options: [
+        { label: '选项1', value: '1' },
+        { label: '选项2', value: '2' }
+      ]
+
+    }
   }
 })
-export default class Home extends Vue {}
 </script>
