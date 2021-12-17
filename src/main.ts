a49bf25c601b520b079364362a345cda353767d3
chengzhile <7354862+zhixuli@user.noreply.gitee.com>
2021-12-17 10:57:17 +0800

    element-plus样式依赖引入

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import '@/styles/element-variables.scss'
 
 const app = createApp(App)
 app.use(ElementPlus, { size: 'medium' })
